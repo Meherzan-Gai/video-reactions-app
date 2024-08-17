3b492f3d7f29ee60c8035fba76034effaeab28cf
Meherzan Gai <meherzan.gai@gmail.com>
2024-08-16 23:55:49 -0400

    Lots of changes, added video component and reaction camera screen. All just basic functionality to then build off of

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,48 @@
-import { Text, View } from "react-native";
+/* Home Screen which will display a list of videos that the user can click on and then watch */
 
+//Import statements
+import { Text, Button, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link } from "expo-router";
+import { VideoView, useVideoPlayer } from "expo-video";
+import Video from "./components/video";
+
+// returns two sections, the header and the body, header is fixed in place
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen. Okay!</Text>
-    </View>
+    <>
+      <SafeAreaView style={styles.header}>
+        <Text style={styles.headerText}>Music Discovery</Text>
+      </SafeAreaView>
+
+      <SafeAreaView style={styles.body}>
+        <Link href="/reactions" asChild>
+          <Button title="Reactions"></Button>
+        </Link>
+
+        <Video></Video>
+      </SafeAreaView>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    alignItems: "center",
+    justifyContent: "center",
+    verticalAlign: "middle",
+    backgroundColor: "#FFFFFF",
+    height: 60,
+  },
+
+  headerText: {
+    fontSize: 25,
+    alignSelf: "center",
+    textAlignVertical: "top",
+  },
+
+  body: {
+    backgroundColor: "#8EF743",
+    flex: 1,
+  },
+});
